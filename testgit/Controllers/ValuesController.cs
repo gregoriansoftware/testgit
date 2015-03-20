1a1920784367fd9cea17fa4606c41cd5e7545e44
gregoriansoftware <solutions@gregoriansoftware.com>
2015-03-20 17:22:22 -0400

    added tracy

--- testgit/Controllers/ValuesController.cs
+++ testgit/Controllers/ValuesController.cs
@@ -12,7 +12,7 @@ namespace testgit.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "greg", "gary" };
+            return new string[] { "greg", "gary", "tracy" };
         }
 
         // GET api/values/5
